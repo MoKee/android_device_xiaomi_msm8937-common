ba38f8af5c5caf73ce2728dc440b612309b781e9
Isaac Chen <isaacchen@isaacchen.cn>
2018-04-06 14:30:06 +0200

    msm8937-common: Mokee-ify
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

--- board/hardware.mk
+++ board/hardware.mk
@@ -1,5 +1,5 @@
 # CM Hardware
 BOARD_USES_CYANOGEN_HARDWARE := true
 BOARD_HARDWARE_CLASS += \
-    hardware/cyanogen/cmhw \
-    device/xiaomi/land/cmhw
+    hardware/mokee/mkhw \
+    device/xiaomi/msm8937-common/mkhw
